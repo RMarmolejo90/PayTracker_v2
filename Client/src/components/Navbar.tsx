48b7bf5849c89e7a711eec5ebe157ccb0dd6cff8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-06-26 22:25:31 -0400

    tryin some different styles out

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -5,27 +5,27 @@ import PayTracker from '../pages/PayTracker';
 
 export default function Navbar() {
   return (
-    <div>
-        <h1>Rich Marmolejo</h1>
-        <ul>
-            <li>
+    <div className='flex flex-row justify-between p-6 min-w-screen bg-primary-gradient'>
+        <h1 className='font-semibold tracking-wider text-zinc-800 text-lg px-6 mr-auto'>Rich Marmolejo</h1>
+        <ul className='flex flex-row justify-between'>
+            <li className='px-6'>
                 <NavLink 
                     to='/' 
-                    className={({ isActive }) => isActive ? 'text-red-500' : 'text-blue-500'}>
+                    className={({ isActive }) => isActive ? 'text-zinc-700 px-4 py-2 border-b-2 rounded-xl' : 'text-blue-500'}>
                     Home
                 </NavLink>
             </li>
-            <li>
+            <li className='px-6'>
                 <NavLink 
                     to="/PayTracker"
-                    className={({ isActive }) => isActive ? 'text-red-500' : 'text-blue-500'}>
+                    className={({ isActive }) => isActive ? 'text-zinc-700 px-4 py-2 border-secondary-gradient rounded-xl' : 'text-blue-500'}>
                     PayTracker
                 </NavLink>
             </li>
-            <li>
+            <li className='px-6'>
                 <NavLink 
                     to="/Dev"
-                    className={({ isActive }) => isActive ? 'text-red-500' : 'text-blue-500'}>           
+                    className={({ isActive }) => isActive ? 'text-zinc-700 px-4 py-2 border-secondary-gradient rounded-xl' : 'text-blue-500'}>           
                     Development
                 </NavLink>
             </li>
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       backgroundImage:{
         'gears': "url('./src/assets/Gears.webp')",
+        'secondary-gradient': "radial-gradient( circle farthest-corner at 17.1% 22.8%,  rgba(226,24,24,1) 0%, rgba(160,6,6,1) 90% );",
+        'primary-gradient': "linear-gradient( 179deg,  rgba(0,0,0,1) 9.2%, rgba(127,16,16,1) 103.9% );"
       }
     },
   },
