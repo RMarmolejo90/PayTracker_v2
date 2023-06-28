02edb6387e87c4e38281e5f726bf451c38ff4d12
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-06-27 21:46:51 -0400

    building the register form

--- Client/src/pages/Register.tsx
+++ Client/src/pages/Register.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Register() {
+    const [newUser, setNewUser] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:""
+    });
+
+    const handleChange = (e) => {
+        setNewUser({ ...newUser, [e.target.name]: e.target.value });
+      };
+    
+      const submit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          // Send a POST request to the API endpoint
+          const response = await axios.post("http://localhost:3000/register", newUser);
+          console.log(response.data); // Optional: Log the response
+    
+          // Reset the form after successful registration
+          setNewUser({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+          });
+        } catch (error) {
+          console.error(error);
+          // Handle error response if needed
+        }
+      };
+    
+
   return (
     <div>
-        <form action="" method="post">
-            <input type="text" name="firstName" id="firstName" />
-            <input type="text" name="lastName" id="lastName" />
-            <input type="email" name="email" id="email" />
-            <input type="password" name="password" id="password" />
+        <form action="POST" method="post" onSubmit={submit}>
+            <input onChange={handleChange} placeholder='First Name' type="text" name="firstName" id="firstName" />
+            <input onChange={handleChange} placeholder='Last Name' type="text" name="lastName" id="lastName" />
+            <input onChange={handleChange} placeholder='Email' type="email" name="email" id="email" />
+            <input onChange={handleChange} placeholder='Password' type="password" name="password" id="password" />
+            <button type="submit">Submit</button>
         </form>
     </div>
   )
