da2f854dbe43719fe806a2ddf5a14908b766f45f
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-10 09:41:15 -0400

    introducing router layout

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div className='bg-gears bg-cover h-screen'>
         <Navbar />
-        Home
+        <Outlet />
     </div>
   )
 }
