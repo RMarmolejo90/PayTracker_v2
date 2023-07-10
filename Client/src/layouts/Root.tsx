73761c0ff7dda8109abeab0cbf5c1e441031d157
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-10 11:45:41 -0400

    implemented nested routing

--- Client/src/layouts/Root.tsx
+++ Client/src/layouts/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
-export default function Home() {
+export default function Root() {
   return (
     <div className='bg-gears bg-cover h-screen'>
         <Navbar />
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,4 +1,4 @@
-import Home from './pages/Home.tsx'
+import Root from './layouts/Root.tsx'
 import PayTracker from './pages/PayTracker.tsx'
 import Dev from './pages/Dev.tsx'
 import ErrorPage from './pages/ErrorPage.tsx'
@@ -14,32 +14,31 @@ import * as React from 'react'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/PayTracker",
-    element: <PayTracker />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/Dev",
-    element: <Dev />,
-    errorElement: <ErrorPage />
-  },
-  
-  {
-    path: "/Login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/Register",
-    element: <Register />,
-    errorElement: <ErrorPage />
-  },
-
-  
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: "/Login",
+        element: <Login />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/PayTracker",
+        element: <PayTracker />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/Dev",
+        element: <Dev />,
+        errorElement: <ErrorPage />
+      },   
+    ]
+  }, 
 ]);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
