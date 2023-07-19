e1d6426bc6744f45d3e374f60e53ba2c7af0176f
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-19 19:58:28 -0400

    added Basic Timer

---
+++ Client/src/components/BasicComponents/BasicTimer.tsx
@@ -0,0 +1,33 @@
+
+interface TimerProps {
+  hours: number;
+  minutes: number;
+  seconds: number;
+  handleStartClick: () => void;
+  handleStopClick: () => void;
+  isActive: boolean;
+}
+
+const BasicTimer: React.FC<TimerProps> = ({ hours, minutes, seconds, handleStartClick, handleStopClick, isActive }) => {
+  return (
+    <div className='m-6 flex flex-row items-center justify-between'>
+      <p className='font-semibold text-2xl p-4 text-center'>
+        {hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
+      </p>
+      <div className='p-4'>
+        {/* start/stop button conditionally rendered */}
+        {localStorage.getItem('activeTimer') === 'true' ? (
+          <button className='bg-red-600 m-4 py-1.5 px-6 font-semibold border-slate-700 rounded-md text-slate-50' onClick={handleStopClick}>
+            Stop
+          </button>
+        ) : (
+          <button className='bg-green-500 m-4 py-1.5 px-6 font-semibold border-slate-700 rounded-md text-slate-50' onClick={handleStartClick}>
+            Start
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BasicTimer;
