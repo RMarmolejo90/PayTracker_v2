d23a1fead80831938fd442d70e7d4c9e27f478dd
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-09-28 06:33:31 -0400

    include delete button for shifts

--- Client/src/components/ProComponents/History.tsx
+++ Client/src/components/ProComponents/History.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 
 type Shift = {
     timeIn: number, 
@@ -16,7 +17,19 @@ type HistoryProps = {
 
 export default function History(props:HistoryProps) {
 
-
+    // delete shift
+    const deleteShift = (_id: string) => {
+        async () => {
+            try {
+            await axios.delete(`http://localhost:3000/shift/${_id}`);
+            console.log(`Shift with _id ${_id} deleted successfully.`);
+            } catch (error) {
+            console.error(`Error deleting shift with _id ${_id}:`, error);
+            }
+        }
+      };
+      
+    
     // raw data from the database
     const rawHistory: Shift[] = props.history;
     
@@ -41,12 +54,13 @@ export default function History(props:HistoryProps) {
             <tbody>
               {history.map((shift) => (
                 <tr key={shift._id}>
-                  <td className="p-2 border-r border-slate-800 ">{new Date(shift.timeIn).toLocaleString()}</td>
-                  <td className="p-2 border-r border-slate-800 ">{shift.endTime ? new Date(shift.endTime).toLocaleString() : '-'}</td>
-                  <td className="p-2 border-r border-slate-800 ">${shift.grossPay}</td>
-                  <td className="p-2 border-r border-slate-800 ">${shift.netPay}</td>
+                  <td className="p-2 border-r border-slate-800 ">{new Date(shift.timeIn).toLocaleString([], { hour: '2-digit', minute: '2-digit' })}</td>
+                  <td className="p-2 border-r border-slate-800 ">{shift.endTime ? new Date(shift.endTime).toLocaleString([], { hour: '2-digit', minute: '2-digit' }) : '-'}</td>
+                  <td className="p-2 border-r border-slate-800 ">${shift.grossPay.toFixed(2)}</td>
+                  <td className="p-2 border-r border-slate-800 ">${shift.netPay.toFixed(2)}</td>
                   <td className="p-2 border-r border-slate-800 ">{shift.hoursWorked}</td>
-                  <td className="p-2 ">{shift.date}</td>
+                  <td className="p-2 ">{new Date(shift.date).toLocaleDateString([], {year:'numeric', month: '2-digit', day: '2-digit'})}</td>
+                  <button onClick={() => deleteShift(shift._id)} className="p-2 bg-slate-400 tracking-wider uppercase text-xs">Delete</button>
                 </tr>
               ))}
             </tbody>
