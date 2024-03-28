c21fad0de6d55a538b4b14397840723f0dfaef25
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-28 13:04:59 -0400

    updated the content on home page

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -4,8 +4,7 @@ export default function Home() {
     <div className="flex flex-row flex-wrap flex-shrink items-center justify-evenly p-6 text-slate-200">
       <div className="flex flex-col flex-wrap items-between justify-start max-w-xl md:m-2">
         <h1 className="text-3xl lg:text-4xl font-semibold p-3 tracking-wide">
-          Expert HVAC Technician. <br />
-          Full-Stack Software Developer.       
+          Engineering Full-Stack Software Solutions       
         </h1>
         <div className="p-2 max-w-md py-4 text-lg">
           <p className='p-1'>
@@ -20,10 +19,10 @@ export default function Home() {
       <section className='lg:m-10 m-6 lg:p-8 p-4 bg-secondary-gradient text-slate-300 border border-slate-950 rounded-md'>
       <h3 className='p-4 font-semibold text-lg'>Welcome to RichMarmolejo.com</h3>
       <p className='p-4'>
-      Here, you'll find information about my expertise as an HVAC technician and my experience as a full-stack software developer. This platform also serves as a hub for my PayTracker app, which is free to use. Stay tuned for exciting updates as this website continues to grow.
+      Hey! My name is Rich. I enjoy developing software programs that make our daily lives easier and more productive. I am also an HVAC Technician. Working on the road as a technician, I would often find myself tracking my work hours and writing them down in a notepad. After my shift I would add up my hours, calculate my deductions and estimate what my paycheck might look like. So then I thought, wouldn't it be great if I could clock in and out on an app that would show me these calculations in real-time? I find it motivating throughout the week to check on my progress, but also helpful to double check the payroll accuracy. So this is what I came up with.
       </p>
       <p className='p-4'>
-      If you'd like to try out the PayTracker app, feel free to create a user account. For any questions or concerns, please don't hesitate to reach out to me. I try to provide prompt responses, typically within 24 hours.
+      If you plan to use the tracker to save your work history, feel free to create a user account. The Pro version will save all the data to a database for you. If you just want to check your work hours and pay calculations in real-time, you can do that from either the basic or the pro versions. Thanks you for trying it out, I hope you find it useful. If you have any questions or feedback, please don't hesitate to reach out to me. I try to provide prompt responses, typically within 24 hours.
       </p>
       <div className='flex items-center justify-center'>
         <a className='text-center m-8 rounded-md hover:bg-yellow-400 p-2 bg-yellow-500 text-slate-950 font-semibold tracking-wider' href="mailto:richardmarmolejo@gmail.com">CONTACT ME HERE</a>
