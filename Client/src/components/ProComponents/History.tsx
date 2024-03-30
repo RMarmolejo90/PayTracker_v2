33b839d666bee31bca91d9d3f119ed493a4ffb5d
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-30 08:49:52 -0400

    update readme

--- Client/src/components/ProComponents/History.tsx
+++ Client/src/components/ProComponents/History.tsx
@@ -39,7 +39,7 @@ export default function History(props:HistoryProps) {
   return (
     <div className="min-w-full p-10 bg-zinc-100 border-orange-500 rounded-xl border-t-2 flex flex-col items-center justify-between">
         <h2 className="font-semibold text-sky-600 text-3xl text-center">Work History</h2>
-        <div>
+        <div className="">
           <table className="">
             <thead>
               <tr className="">
@@ -61,7 +61,7 @@ export default function History(props:HistoryProps) {
                     <td className="p-2 border-r border-slate-800 ">{shift.hoursWorked}</td>
                     <td className="p-2 ">{new Date(shift.date).toLocaleDateString([], {year:'numeric', month: '2-digit', day: '2-digit'})}</td>
                     <td className="p-2">
-                        <button type="button" onClick={() => deleteShift(shift._id)} className="bg-slate-800 tracking-wider uppercase text-xs p-2 font-semibold text-slate-100 hover:cursor-pointer hover:bg-red-700 rounded-xl">Delete</button>
+                    <button type="button" onClick={() => deleteShift(shift._id)} className="bg-slate-800 tracking-wider uppercase text-xs p-2 font-semibold text-slate-100 hover:cursor-pointer hover:bg-red-700 rounded-xl">Delete</button>
                     </td>                
                 </tr>
               ))}
